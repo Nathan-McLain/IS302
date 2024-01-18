46b38a0feb5941e3252106139f81f89e6e3726f1
Nathan McLain <nathanmclain@live.com>
2024-01-18 09:47:22 -0800

    test4

---
+++ newName/src/NavBar.js
@@ -0,0 +1,16 @@
+
+export function NavBar() {
+    return <>
+        <nav id="mobile_menu"></nav>
+        <nav id="nav_menu">
+            <ul>
+                <li><a href="../Html/index.html"><i className="fa-solid fa-house"></i> Home</a></li>
+                <li><a href="../Html/nathans-page.html"><i className="fa-regular fa-folder-open"></i> Nathan</a></li>
+                <li><a href="../Html/zacks-page.html"> Zack</a></li>
+                <li><a href="../Html/roberts-page.html"> Robert</a></li>
+                <li><a href="#"><i className="fa-regular fa-address-book"></i> Contact information</a></li>
+            </ul>
+        </nav>
+
+    </>
+}
